7f298419f9994c446803ff6dc9803c06dd282d34
Iyed Ben Rejeb <iyed.exe@gmail.com>
2024-03-27 11:02:03 +0100

    lot size handing and doc

--- README.md
+++ README.md
@@ -1,50 +1,64 @@
 # TriangularArbitragePyBot
+# Implementation choices :
 
+## Order size:
+when we say we BUY(<-) a PAIR USD/EUR we are buying the base, ie USD and selling the quote, ie EUR.
+==> we are spending EUR
+when we say we SELL(->) a PAIR USD/EUR we are selling the base, ie USD and buying the quote, ie EUR.
+==> we are spending USD
+
+this resulted in the choice where 
+    - buy orders are sized in quote currency
+    - sell orders are sized in base currency
+
+## Strategy profitability evaluation:
+Follow the simple example of the cycle based on BNB, BTC and ETH in that order :
+
+BNB -> BTC -> ETH -> BNB            
+a possible path is :
+SELL BNB/BTC
+    - unitary cost : 1 BNB * (100 - Fee1%)/100
+    - get : Y1 BTC
+BUY ETH/BTC
+    - unitairy cost : 1 BTC * Fee2%
+    - get : 1/X2 ETH
+SELL ETH/BNB
+    - unitairy cost : 1 ETH * Fee3%
+    - get : Y3 BNB
+
+==> FinalBNB = StartingBNB * (100 - Fee1%) * Y1 * (100 - Fee2%) * 1/X2 * (100 - Fee3%) * Y3
+r is the profitabilty of a cycle :
+r = (100 - Fee1%) * Y1 * (100 - Fee2%) * 1/X2 * (100 - Fee3%) * Y3
+money is made if r>1
+
+# References :
 use : 
-client webstream implementation:
+## client webstream implementation:
 https://github.com/binance/binance-signature-examples/blob/master/python/websocket-api/websocket_api_client.py
 
-ws content:
+## ws content:
 https://github.com/binance/binance-spot-api-docs/blob/master/web-socket-streams.md
 
-
-good inspiration:
+## good inspiration:
 https://dev.to/ken_mwaura1/crypto-data-bot-using-python-binance-websockets-and-postgresql-db-5fnd
 
-potential improvement:
+## potential improvement:
 https://www.thealgorists.com/Algo/ShortestPaths/Arbitrage
 
-How to setup :
+## APIs used :
+https://docs.aiohttp.org/en/stable/client_reference.html
+https://docs.python.org/3/library/asyncio-sync.html#asyncio.Event
+https://binance-docs.github.io/apidocs/websocket_api/en/#signed-request-example-rsa
+
+# How to setup :
 >python -m venv dodrio
 windows : >venv\dodrio\Scripts\activate.bat
 linux :
 >source venv/dodrio/bin/activate
 >pip install -r requirements.txt
 
-
-# sell to the bid
-# buy from the ask
-
-# bid X, ask Y
-
-# BNB -> BTC -> ETH -> BNB            
-
-# SELL BNB/BTC
-# cost : 1 BNB * (100 - Fee1%)/100
-# get : Y1 BTC
-
-# BUY ETH/BTC
-# cost : 1 BTC * Fee2%
-# get : 1/X2 ETH
-
-# SELL ETH/BNB
-# cost : 1 ETH * Fee3%
-# get : Y3 BNB
-
-# FinalBNB = StartingBNB * (100 - Fee1%) * Y1 * (100 - Fee2%) * 1/X2 * (100 - Fee3%) * Y3
-
-
-#Important : 
+# General doc
+## Binance server time sync : 
 Timing security
 
     SIGNED requests also require a timestamp parameter which should be the current millisecond timestamp.
@@ -63,7 +77,31 @@ Serious trading is about timing. Networks can be unstable and unreliable, which 
 
 It is recommended to use a small recvWindow of 5000 or less!
 
-#
+## Binance LOT SIZE
+LOT_SIZE
+
+    ExchangeInfo format:
+
+  {
+    "filterType": "LOT_SIZE",
+    "minQty": "0.00100000",
+    "maxQty": "100000.00000000",
+    "stepSize": "0.00100000"
+  }
+
+The LOT_SIZE filter defines the quantity (aka "lots" in auction terms) rules for a symbol. There are 3 parts:
+
+    minQty defines the minimum quantity/icebergQty allowed.
+    maxQty defines the maximum quantity/icebergQty allowed.
+    stepSize defines the intervals that a quantity/icebergQty can be increased/decreased by.
+
+In order to pass the lot size, the following must be true for quantity/icebergQty:
+
+    quantity >= minQty
+    quantity <= maxQty
+    quantity % stepSize == 0
+
+# FX Pairs
 Base Currency
 The base currency is the first currency listed in a currency pair. It is the currency that is being bought or sold. For example, in the EUR/USD currency pair, the euro (EUR) is the base currency.
 
--- app/order_handler.py
+++ app/order_handler.py
@@ -7,6 +7,7 @@ import json
 import uuid
 import configparser
 import time
+from collections import defaultdict 
 from financial_objects import Order, Signal, Way, CoinPair, OrderType
 from utils import get_timestamp, signal_handler, init_logger, compute_signature
 
@@ -32,79 +33,116 @@ class OrderHandler:
         self._websocket_endpoint = config[market_connection]['websocket_endpoint']
         self._binance_api_key = config[market_connection]['api_key']
         self._binance_key_secret = config[market_connection]['key_secret']
+        self.balance = defaultdict(float)
+        self.lot_size = defaultdict(float) 
+        self.min_size = defaultdict(float)
+        self.max_size = defaultdict(float)
 
-        self._conditions = {}
-        self._message_response = {}
-        self._bot = None
-        # self.start_listen=asyncio.Event()
+
+    async def listen_socket(self):
+        wsMessage = await self.ws.receive()
+        if wsMessage.type == aiohttp.WSMsgType.ERROR:
+            log.critical(f"Error message received : [{wsMessage}]")
+            return None
+        if wsMessage.type == aiohttp.WSMsgType.TEXT:
+            return wsMessage.data 
 
     async def send_command(self, command, *args):
         message_id = str(uuid.uuid4())
-        self._conditions[message_id] = asyncio.Event()
         object_res = command(message_id, *args)
-        log.warning(object_res)
         message_to_server = json.dumps(object_res)
         await self.ws.send_str(message_to_server)
         return message_id
     
     async def wait_response(self, message_id):
-        log.warning(f'waiting for {message_id}')
-        await self._conditions[message_id].wait()
-        response = self._message_response.pop(message_id)
-        del self._conditions[message_id]
-        if response.get('status') != STATUS_OK:
-            raise Exception(f"[BNB] Response NOK [{response.get('error')}]")
-        return response.get('result')    
+        log.warning(f'Waiting for response for {message_id}')
+        raw_response = await self.listen_socket()
+        log.debug(f'raw response : {raw_response}')    
+        message_json = json.loads(raw_response)
+        rmessage_id = message_json.get('id')
+        message_result = message_json.get('result')
+        if rmessage_id is None or rmessage_id != message_id:
+            raise Exception(f"[BNB] Recieved message id [{rmessage_id}] different than awaited [{message_id}]")
+        if message_json.get('status') != STATUS_OK:
+            raise Exception(f"[BNB] Market reject, reason [{message_json.get('error')}]")
+        return message_result 
 
     async def process_signal(self, signal: Signal):
-        orders_list = signal.get_orders_list()
-        log.info(f"Signal received:\n"
-              f"\tTheo unitairy pnl = [{signal.get_theo_pnl()}],\n" 
-              f"\tDescription = [{signal.get_description()}]")
+        try:
+            orders_list = signal.get_orders_list()
+            log.info(f"Signal received:\n"
+                f"\tTheo unitairy pnl = [{signal.get_theo_pnl()}],\n" 
+                f"\tDescription = [{signal.get_description()}]")
 
-        for order in orders_list:
+            for order in orders_list:
 
-            pair = order.get_pair()
-            if order.get_way() == Way.BUY:
-                starting_coin = pair.get_quote_currency()
-            elif order.get_way() == Way.SELL:
-                starting_coin = pair.get_base_currency()
+                
+                
+                await self.execute_order(order)
 
-            balance = await self.get_balance(starting_coin)
-            log.info(f'Current balance of {starting_coin} : {balance}')
+                log.info(f'Finshed executing order')
+        except Exception as e:
+            log.critical('An exception occured during signal processing')
+            log.exception(e)
+            exit(1)
+    
+    def set_size(self, order: Order):
+        pair = order.get_pair()
+        if order.get_way() == Way.BUY:
+            starting_coin = pair.get_quote_currency()
+        elif order.get_way() == Way.SELL:
+            starting_coin = pair.get_base_currency()
+
+        if order.get_quantity() == 0:
+            qty = self.balance[starting_coin]
+        else:
+            qty = order.get_quantity()
+
+        self.lot_size[symbol] 
+        self.min_size[symbol] = filter_obj['minQty']
+        self.max_size[symbol] = filter_obj['maxQty']
+
+        if qty >= self
             
-            await self.execute_order(order)
 
-            balance = await self.get_balance(starting_coin)
-            log.info(f'Current balance of {starting_coin} : {balance}')
-
-            log.info(f'Finshed executing order')
-
-    async def get_balance(self, asset):
-        log.info(f'Getting balance for {asset}')
+    async def get_balances(self):
         message_id = await self.send_command(self.get_account_status_command)
-        await self.listen_socket()
-        accountStatus = await self.wait_response(message_id)
-
-        balance = 0
-        for balanceJson in accountStatus.get('balances'):
-            if balanceJson.get('asset') == asset:
-                balance=float(balanceJson.get('free'))
-        return balance
+        account_status = await self.wait_response(message_id)
+        
+        for asset_balance_obj in account_status.get('balances'):
+            self.balance[str(asset_balance_obj['asset'])]=float(asset_balance_obj['free'])
+            log.debug(f'Current balance of [{asset_balance_obj["asset"]}] == [{asset_balance_obj["free"]}]')
     
     async def execute_order(self, order:Order):
         if order.getType() != OrderType.MARKET:
             log.error(f'Unhandled order type : {order.getType()}')
+        log.info(f'Sending market order way=[{order.get_way()}] symbol=[{order.get_symbol()}] qty=[{order.get_quantity()}]')
+
         message_id = await self.send_command(self.place_order_command, order)
-        await self.listen_socket()
         exec_response = await self.wait_response(message_id)
+
         log.info(f'ack_response : {exec_response}')
         log.info('Finished')
-        
-        # await self.listen_socket()
-        # exec_response = await self.wait_response(message_id)
-        # print(exec_response)
     
+    async def init_lot_sizes(self):
+        log.info('Getting exchange info')
+        message_id = await self.send_command(self.get_exchange_info_command)
+        exchange_info = await self.wait_response(message_id)
+        log.info('Initializing lot sizes')
+        for symbol_obj in exchange_info['symbols']:
+            symbol = symbol_obj['symbol']
+            for filter_obj in symbol_obj['filters']:
+                if filter_obj['filterType'] == 'LOT_SIZE':
+                    self.lot_size[symbol] = filter_obj['stepSize'] 
+                    self.min_size[symbol] = filter_obj['minQty']
+                    self.max_size[symbol] = filter_obj['maxQty']
+
+    def get_exchange_info_command(self,message_id):
+        return {
+            "id": message_id,
+            "method": "exchangeInfo"
+        }  
+            
     def get_account_status_command(self,message_id):
         timestamp = get_timestamp()
         payload = {
@@ -142,30 +180,14 @@ class OrderHandler:
             "method": "order.place",
             "params": payload
         }
-
-    async def listener_run_(self):
-        await self.start_listen.wait()
-        while True:
-            await self.listen_socket()
-
-    def listener_run(self):
-        asyncio.run(self.listener_run_())
-        
-    async def listen_socket(self):
-        wsMessage = await self.ws.receive()
-        # log.critical(wsMessage)
-        if wsMessage.type == aiohttp.WSMsgType.ERROR:
-            self.on_error(wsMessage)
-
-        if wsMessage.type == aiohttp.WSMsgType.TEXT:
-            txtMessage = wsMessage.data
-            self.on_message(txtMessage)
-    
+                        
     async def on_open(self):
         log.info('WS connection opened ..')
-        # listener = threading.Thread(target=self.listener_run)
-        # listener.start()
+        await self.init_lot_sizes()
+        input()
         while True:
+            log.info(f'Reinitializing portfolio, getting coin balances')
+            await self.get_balances()
             log.info('Listening to signals queue ..')
             signal = self.q.get()
             log.info('Caught new signal ..')
@@ -173,25 +195,6 @@ class OrderHandler:
                 break
             await self.process_signal(signal)
             
-
-    def on_message(self, message):
-        message_json = json.loads(message)
-        message_id = message_json.get('id')
-        if message_id is None:
-            log.error(f'Message received with no id : {message}')
-            return
-        self._message_response[message_id] = message_json
-        self._conditions[message_id].set()
-
-
-    def on_error(self, error):
-        log.critical(f'Received error message : {error}')
-
-    def on_close(self, close_status_code, close_msg):
-        log.warning("Connection close")
-        log.info(close_status_code)
-        log.info(close_msg)
-
     async def run_(self, q :multiprocessing.Queue):
         self.q = q
         log.info('Order handler running ..')
@@ -226,7 +229,7 @@ def main():
     #     1
     # )
 
-    path=[Order(CoinPair("BNB", "USDT"), Way.SELL, type=OrderType.MARKET, quantity=0.01)]
+    path=[Order(CoinPair("REEF", "USDT"), Way.SELL, type=OrderType.MARKET, quantity=0.01)]
     signal = Signal(
         path,
         "SELL REEF",
